19af50c069f706323fdd8b628e0cf148057d1c8a
Pavel <romb32@yandex.ru>
2020-09-16 09:55:15 +0300

    6 commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 /* ====== Фильтры ===== */
 
-/* Фильтры по содержимому в jQuery */		
+/* Фильтры форм в jQuery */		
 
-// $('li:contains(файл)').css('border', 'solid 1px red'); // содержит текст
-// $('p:empty').text('Любой текст')  // пустой
-// $('h2:has(span)').css('border', '1px solid #000'); // имеет внутри селектор
-// $('span:parent').css('border', '1px solid #000'); // обращение к родителю
+// $('input').css('border', '1px solid red'); // Все поля input 
+// $('input:button').css('border', '1px solid red'); // Поле button - простая кнопка
+// $('input:radio').css('display', 'block'); // Радио-кнопки 
+// $('input:checkbox').css('display', 'block'); // Чекбоксы
+// $('input:text').css('border', '1px solid red');// Текстовые поля для ввода
+// $('input:password').css('border', '1px solid red'); // Поле для ввода пароля
+// $('input:file').css('border', '1px solid red'); // Поле для загрузки файла
+// $('input:submit').css('border', '1px solid red'); // Кнопка "Отправить"
+// $('input:reset').css('border', '1px solid red'); // Кнопка "Сбросить"
+// $('input:image').css('border', '1px solid red'); // Кнопка-картинка 
+// $('input:focus').css('display', 'block'); // Выбранное поле (состояние фокус)
+// $('input:checked').css('border', '1px solid red'); // Выбранный чекбокс или радио-кнопка
+// $('input:enabled').css('border', '1px solid red'); // Доступное поле
+// $('input:disabled').css('border', '1px solid red'); // Недоступное поле
